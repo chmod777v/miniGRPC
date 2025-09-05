d48abd376babd1f0b21201d31fdb52437b8aa39a
chmod777v <ivan.pylaev1@gmail.com>
2025-09-05 23:59:37 +0300

    HttpServerUpdate

---
+++ pkg/httpServer/grpcConect/grpcConect.go
@@ -0,0 +1,40 @@
+package grpcconect
+
+import (
+	"context"
+	"fmt"
+	g_serv "grpc/pkg/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Server struct {
+	grpcClient g_serv.ServiceClient
+	conn       *grpc.ClientConn
+}
+
+func NewServer() *Server {
+	conn, err := grpc.NewClient("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic("Failed to conect to server:" + err.Error())
+	}
+
+	return &Server{
+		grpcClient: g_serv.NewServiceClient(conn),
+		conn:       conn,
+	}
+}
+
+func (s *Server) PostRequestGrpc(ctx context.Context, requestData *g_serv.GetRequest) (*g_serv.GetResponse, error) {
+	response, err := s.grpcClient.Get(ctx, requestData)
+	if err != nil {
+		return nil, fmt.Errorf("gRPC call failed: %w", err)
+	}
+	return response, nil
+}
+func (s *Server) Close() {
+	if s.conn != nil {
+		s.conn.Close()
+	}
+}
--- pkg/httpServer/httpServer.go
+++ pkg/httpServer/httpServer.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	my_grpc "grpc/internal/grpc"
+	grpcconect "grpc/pkg/httpServer/grpcConect"
+	g_serv "grpc/pkg/proto"
 	"io"
 	"log/slog"
 	"net/http"
@@ -12,27 +13,30 @@ import (
 type personID struct {
 	Id int
 }
+type httpServer struct {
+	grpcServer *grpcconect.Server
+}
 
-func GetRequest(body *io.ReadCloser, person interface{}) error {
+func GetRequest(body *io.ReadCloser, person interface{}, w http.ResponseWriter) error {
 	if err := json.NewDecoder(*body).Decode(&person); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		slog.Error("Erro while receiving data", "ERROR", err.Error())
 		return err
 	}
 	return nil
 }
-func handler(w http.ResponseWriter, r *http.Request) {
+func (h *httpServer) handler(w http.ResponseWriter, r *http.Request) {
 	var personID personID
 	var person my_grpc.Person
 	if r.Method == http.MethodGet {
-		if err := GetRequest(&r.Body, &personID); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+		if err := GetRequest(&r.Body, &personID, w); err != nil {
 			return
 		}
 		slog.Info("Request Get", "Data", personID)
-
+		resp, _ := h.grpcServer.PostRequestGrpc(r.Context(), &g_serv.GetRequest{Id: int64(personID.Id)})
+		slog.Info("Response Get", "Data", resp)
 	} else if r.Method == http.MethodPost {
-		if err := GetRequest(&r.Body, &person); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+		if err := GetRequest(&r.Body, &person, w); err != nil {
 			return
 		}
 		slog.Info("Request Post", "Data", person)
@@ -41,8 +45,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Ошибка при запуске сервера")
+	serv := grpcconect.NewServer()
+	defer serv.Close()
+
+	httpServer := &httpServer{grpcServer: serv}
+	http.HandleFunc("/", httpServer.handler)
+
+	if err := http.ListenAndServe(":8090", nil); err != nil {
+		slog.Error("Error starting server", "ERROR", err.Error())
 	}
+	slog.Info("Server started successfully")
 }

7cb29c2698602a2c87d8b6fb3431130c08ccef54
chmod777v <ivan.pylaev1@gmail.com>
2025-09-17 23:13:15 +0300

    Update httpServer.go

--- pkg/httpServer/httpServer_test.go
+++ pkg/httpServer/httpServer_test.go
@@ -1,26 +1,30 @@
 package httpServer
 
 import (
-	"context"
+	"encoding/json"
 	g_serv "grpc/pkg/proto"
 	mock_httptests "grpc/testingHttp/mock"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func TestHandleGet(t *testing.T) {
 	testCases := []struct {
 		name           string
-		inputID        int64
+		inputID        string
 		expectedPerson *g_serv.GetResponse
 		expectedError  error
 		expectedTimes  int
 	}{
 		{
 			name:    "successful get person",
-			inputID: 1,
+			inputID: `{"id": 1}`,
 			expectedPerson: &g_serv.GetResponse{
 				Info: &g_serv.UserInfo{
 					Name:   "John",
@@ -30,13 +34,13 @@ func TestHandleGet(t *testing.T) {
 			expectedError: nil,
 			expectedTimes: 1,
 		},
-		/*{
+		{
 			name:           "error get person",
-			inputID:        2,
+			inputID:        `{"id": 22}`,
 			expectedPerson: nil,
 			expectedError:  status.Error(codes.InvalidArgument, "error processing request"),
-			expectedTimes:  0,
-		},*/
+			expectedTimes:  1,
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -44,21 +48,38 @@ func TestHandleGet(t *testing.T) {
 			defer ctrl.Finish()
 
 			mockGrpc := mock_httptests.NewMockHttpRequestGrpc(ctrl)
-			expectedReq := &g_serv.GetRequest{Id: tc.inputID}
+
+			var responsePerson struct {
+				Id      int
+				User_id int
+				Name    string
+				Admin   bool
+			}
+			json.Unmarshal([]byte(tc.inputID), &responsePerson)
+
+			expectedReq := &g_serv.GetRequest{Id: int64(responsePerson.Id)}
 			mockGrpc.EXPECT().
 				GetRequestGrpc(gomock.Any(), expectedReq).
 				Return(tc.expectedPerson, tc.expectedError).
 				Times(tc.expectedTimes)
 
 			server := &HttpServer{requestGrpc: mockGrpc}
-			req := &g_serv.GetRequest{Id: tc.inputID}
-			resp, err := server.requestGrpc.GetRequestGrpc(context.Background(), req)
 
-			assert.NoError(t, err)
-			assert.NotNil(t, resp)
-			assert.Equal(t, tc.expectedPerson.Info.Name, resp.Info.Name)
-			assert.Equal(t, tc.expectedPerson.Info.UserId, resp.Info.UserId)
-			assert.Equal(t, tc.expectedPerson.Info.Admin, resp.Info.Admin)
+			req := httptest.NewRequest("GET", "/", strings.NewReader(tc.inputID))
+			w := httptest.NewRecorder()
+			server.handleGet(w, req)
+
+			if tc.expectedError != nil {
+				assert.Empty(t, w.Body.String())
+			} else {
+				err := json.Unmarshal(w.Body.Bytes(), &responsePerson)
+				assert.NoError(t, err)
+
+				assert.Equal(t, tc.expectedPerson.Info.Name, responsePerson.Name)
+				assert.Equal(t, int(tc.expectedPerson.Info.UserId), responsePerson.User_id)
+				assert.Equal(t, tc.expectedPerson.Info.Admin, responsePerson.Admin)
+
+			}
 		})
 	}
 }
